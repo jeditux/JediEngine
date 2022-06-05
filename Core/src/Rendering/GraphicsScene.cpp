22a825b8d9dbc9c6f33bfb9997448374789ad024
jeditux <aegostev@gmail.com>
2022-06-05 14:23:47 +0300

    Display multiple objects

--- Core/src/Rendering/GraphicsScene.cpp
+++ Core/src/Rendering/GraphicsScene.cpp
@@ -59,6 +59,19 @@ namespace Rendering {
             -0.5f,  0.5f, -0.5f,  0.0f, 1.0f
     };
 
+    glm::vec3 cubePositions[] = {
+            glm::vec3( 0.0f,  0.0f,  0.0f),
+            glm::vec3( 2.0f,  5.0f, -15.0f),
+            glm::vec3(-1.5f, -2.2f, -2.5f),
+            glm::vec3(-3.8f, -2.0f, -12.3f),
+            glm::vec3( 2.4f, -0.4f, -3.5f),
+            glm::vec3(-1.7f,  3.0f, -7.5f),
+            glm::vec3( 1.3f, -2.0f, -2.5f),
+            glm::vec3( 1.5f,  2.0f, -2.5f),
+            glm::vec3( 1.5f,  0.2f, -1.5f),
+            glm::vec3(-1.3f,  1.0f, -1.5f)
+    };
+
     GraphicsScene::GraphicsScene(std::string executablePath) : m_executablePath(std::move(executablePath)) {
         m_pResourceManager = std::make_unique<Core::ResourceManager>(m_executablePath);
         m_pShaderProgram = m_pResourceManager->loadShader("triangle", "triangle.vert", "triangle.frag");
@@ -99,10 +112,6 @@ namespace Rendering {
         glClearColor(m_backgroundColor[0], m_backgroundColor[1], m_backgroundColor[2], m_backgroundColor[3]);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-        glm::mat4 model = glm::mat4(1.0f);
-        model = glm::rotate(model, static_cast<float>(glfwGetTime()) * glm::radians(50.0f), glm::vec3(0.5f, 1.0f, 0.0f));
-        m_pTextureShader->setMatrix("model", glm::value_ptr(model));
-
         glm::mat4 view = glm::mat4(1.0f);
         view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
         m_pTextureShader->setMatrix("view", glm::value_ptr(view));
@@ -116,7 +125,14 @@ namespace Rendering {
         m_pSmileTexture->bind();
         m_pTextureShader->use();
         glBindVertexArray(m_vao);
-        glDrawArrays(GL_TRIANGLES, 0, 36);
+        for (unsigned int i = 0; i < 10; ++i) {
+            glm::mat4 model = glm::mat4(1.0f);
+            model = glm::translate(model, cubePositions[i]);
+            float angle = 20.0f * i;
+            model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
+            m_pTextureShader->setMatrix("model", glm::value_ptr(model));
+            glDrawArrays(GL_TRIANGLES, 0, 36);
+        }
         glBindVertexArray(0);
     }
 
